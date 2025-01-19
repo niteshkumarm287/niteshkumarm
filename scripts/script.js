d3fa1c8fa053834425bc9d2ba85b97eacf47a25c
Nitesh Kumar M <65917611+niteshkumarm287@users.noreply.github.com>
2025-01-19 15:06:32 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // Typing effect logic
 const typedTextSpan = document.querySelector(".typing");
-const textArray = ["DevOps Engineer", "Cloud Enthusiast", "Kubernetes Specialist"];
+const textArray = ["A DevOps Engineer", "Cloud Enthusiast", "Kubernetes Specialist"];
 const typingDelay = 100;
 const erasingDelay = 50;
 const newTextDelay = 2000; // Delay between current and next text
